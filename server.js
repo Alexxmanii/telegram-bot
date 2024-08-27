8726f89bb1dbd945d691a8fd9b00edc6bea31296
Alex <mohammad.mohammad@dci-student.org>
2024-08-27 15:05:47 +0200

    alex-telegram

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app =express();
 app.use(express.json());
+import TelegramBot from "node-telegram-bot-api";
+const token = "7034264368:AAHppCL7oBA_Dj3vKLkVVM7Mj_kU5PMkRzw";
+const bot = new TelegramBot(token, {polling:true}); 
+
 app.post("/message",(req,res)=>{
     res.status(200).send("Message received");
 });
@@ -8,7 +12,13 @@ app.post("/message",(req,res)=>{
 app.all("/message", (req,res)=>{
  res.status(405).send("Method not Allowed");
 });
-
+bot.on("message",(msg)=>{
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    console.log(`Received message from ${chatId}: ${text}`);
+    //-----send a message back to chat---------
+    bot.sendMessage(chatId,`you said ${text}`);
+})
 const PORT =3006;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
